98bef1b3d33aedc0ffce9193685085d605f92239
wmjtxt <972213032@qq.com>
2018-04-17 06:11:09 -0400

    :grin:add patB1008

---
+++ patB1008.cc
@@ -0,0 +1,14 @@
+ ///
+ /// @file    patB1008.cc
+ /// @author  wmjtxt(972213032@qq.com)
+ /// @date    2018-04-17 06:10:31(NewYork time)
+ /// @from    
+ ///
+ 
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	return 0;
+}
