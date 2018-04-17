1fed16398f2f87621a0107324924d7a8634868a7
wmjtxt <972213032@qq.com>
2018-04-17 18:12:46 +0800

    p8

--- patB1008.cc
+++ patB1008.cc
@@ -8,7 +8,32 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	int n;
-	return 0;
+void rev(int a[],int x, int y){
+    int i;
+    for(i = x; i < (x+y+1)/2; i++){
+      int temp = a[i];
+      a[i] = a[x+y-i];
+      a[x+y-i] = temp;
+    }
+}
+
+int main()
+{
+    int n, m;
+    int i;
+    int a[100];
+    while(cin >> n >> m){
+        if(m > n) m = m % n;
+        if(n >= 1 && n <= 100){
+          for(i = 0; i < n; i++)
+            cin >> a[i];
+          rev(a,0,n-m-1);
+          rev(a,n-m,n-1);
+          rev(a,0,n-1);
+          for(i = 0; i < n-1; i++)
+            cout << a[i] <<" ";
+          cout << a[n-1];
+       }
+    }
+    return 0;
 }
