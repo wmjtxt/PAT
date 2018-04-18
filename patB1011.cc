c0ed35cbcd639bf85674835473f6bcd7a789c438
wmjtxt <972213032@qq.com>
2018-04-18 08:57:40 +0800

    :pencil:Update patB1011.cc

--- patB1011.cc
+++ patB1011.cc
@@ -8,7 +8,23 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	int n;
-	return 0;
+int main()
+{
+    int n;
+    while(cin >> n){
+      long long int A[10], B[10], C[10];
+      int i;
+      for(i = 0; i < n; i++){
+        cin >> A[i] >> B[i] >> C[i];
+      }
+      for(i = 0; i < n; i++){
+          if(A[i] + B[i] > C[i]){
+            cout << "Case #" << i+1 << ": " << "true" << endl;
+          }
+          else{
+            cout << "Case #" << i+1 << ": " << "false" <<endl;
+          }
+      }
+    }
+    return 0;
 }
