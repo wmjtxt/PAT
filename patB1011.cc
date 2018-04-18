4e5e8894e9a8c36ea421d9f28162e850357bf630
wmjtxt <972213032@qq.com>
2018-04-17 20:53:50 -0400

    :memo:patB1011.cc

---
+++ patB1011.cc
@@ -0,0 +1,14 @@
+ ///
+ /// @file    :patB1011.cc
+ /// @author  :wmjtxt(972213032@qq.com)
+ /// @date    :2018-04-17 20:52:36(NewYork time)
+ /// @quote   :
+ ///
+ 
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	return 0;
+}
