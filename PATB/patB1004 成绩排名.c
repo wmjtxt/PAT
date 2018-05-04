25163ecbaa4503b2691076dd5468cb04d43b697b
王梦杰 <972213032@qq.com>
2018-05-04 12:22:36 +0800

    Update patB1004 成绩排名.c

--- PATB/patB1004 成绩排名.c
+++ PATB/patB1004 成绩排名.c
@@ -2,29 +2,35 @@
  /// @file    patB1004.cc
  /// @author  wmjtxt(972213032@qq.com)
  /// @date    2018-04-17 05:01:03(NewYork time)
- /// @quote   liuchuo(柳婼)
+ /// @quote   null
  ///
  
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-	int n,max = -1,min = 101,score;
-	cin >> n;
-	string maxname,minname,maxnum,minnum,name,num;
-	for(int i = 0; i < n; ++i){
-		cin >> name >> num >> score;
-		if(max < score){
-			max = score;
-			maxname = name;
-			maxnum = num;
-		}
-		if(min > score){
-			min = score;
-			minname = name;
-			minnum = num;
-		}
-	}
-	cout << maxname << " " << maxnum << "\n" << minname << " " << minnum << endl;;
-	return 0;
+struct E{
+  char name[11];
+  char stu_id[11];
+  int grade;
+}buf[1000];
+
+int main()
+{
+    int n;
+    while(scanf("%d", &n) != EOF){
+        int i, j, k;
+        int imax = 0, imin = 0;
+        for(i = 0; i < n; i++){
+            scanf("%s%s%d",buf[i].name, buf[i].stu_id, &buf[i].grade);
+        }//input
+        for(j = 1; j < n; j++){
+            if(buf[j].grade > buf[imax].grade) imax = j;
+        }//search max
+        for(k = 1; k < n; k++){
+            if(buf[k].grade < buf[imin].grade) imin = k;
+        }//search min
+        printf("%s %s\n", buf[imax].name, buf[imax].stu_id);
+        printf("%s %s",buf[imin].name, buf[imin].stu_id);
+    }
+    return 0;
 }
